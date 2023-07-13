f0f5638202956a84d518d45432ff6d89b7d8d623
Abhishek Jaswal <abhishek.jaswal.1999@gmail.com>
2023-07-13 23:40:37 +0530

    dsfS

--- client/src/modules/Dashboard/index.js
+++ client/src/modules/Dashboard/index.js
@@ -40,7 +40,7 @@ const Dashboard = (props) => {
   console.log("messages--->", messages)
   useEffect(() => {
     //Socket
-    setSocket(io(`${process.env.REACT_SOCKET_URL}`));
+    setSocket(io(`${process.env.REACT_SOCKET_BACKEND_URL}`));
   }, []);
 
 
